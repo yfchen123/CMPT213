1b6ec9517e01da13ee2edf151f8b5deed4d44f1f
Korban910 <korban.torsun@gmail.com>
2021-10-16 10:44:21 -0700

    Iterator example.

---
+++ IteratorsExample/src/IteratorClass.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorClass {
+
+    public static void main(String[] args) {
+
+        List<String> data = new ArrayList<>();
+        data.add("Objected-Oriented");
+        data.add("Design");
+        data.add("in");
+        data.add("Java");
+
+        //IMPORTANT
+        Iterator<String> dataltr = data.iterator();
+        while(dataltr.hasNext())
+        {
+            String temp = dataltr.next();
+            if(temp.contains("i"))
+            {
+                System.out.printf("%s%n", temp);
+            }
+        }
+    }
+}
