a9aacef187d638d75b6f74508915dbea01f1b25f
Korban910 <korban.torsun@gmail.com>
2021-10-28 17:27:50 -0700

    Book class has been created.

---
+++ Inheritance/src/Book.java
@@ -0,0 +1,20 @@
+public class Book {
+
+    private int pages;
+
+    public Book() {
+        this.pages = 0;
+    }
+
+    public Book(int pages) {
+        this.pages = pages;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
