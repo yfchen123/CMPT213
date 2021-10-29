40767528ec37b17f9d62480216ead3e2e77bf2a2
Korban910 <korban.torsun@gmail.com>
2021-10-28 17:36:23 -0700

    Tester and Dictionary class has been created.

---
+++ Inheritance/src/Dictionary.java
@@ -0,0 +1,43 @@
+public class Dictionary extends Book{
+
+    // do NOT shadow copy your parent
+    // 1. if your parent has it, just use it
+    // 2. if you need create it for yourself ONLY (do NOT assume parent knows it)
+    private int definitions;
+
+    public Dictionary() {
+        this.definitions = 0;
+    }
+
+    public Dictionary(int definitions) {
+        this.definitions = definitions;
+    }
+
+    public Dictionary(int pages, int definitions) {
+        super(pages);
+        this.definitions = definitions;
+    }
+
+    public int getDefinitions() {
+        return definitions;
+    }
+
+    public void setDefinitions(int definitions) {
+        this.definitions = definitions;
+    }
+
+    /*
+     * explain why you return -Anything
+     */
+    public double computeRatio() {
+        int temp = super.getPages();
+        if(temp > 0)
+            return this.definitions / super.getPages();
+        return -1;
+    }
+
+    // please do NOT create the methods already exist in your parent
+   /* public int getPages() {
+        return super.getPages();
+    }*/
+}
---
+++ Inheritance/src/Tester.java
@@ -0,0 +1,8 @@
+public class Tester {
+
+    public static void main(String[] args) {
+
+        Dictionary newOne = new Dictionary(100, 500);
+        System.out.println(newOne.getPages());
+    }
+}
